c6a47016444cad4ca8ff51968bbc3907c0991498
Magnus N Malmquist <magnusnym97@gmail.com>
2021-02-28 18:03:26 +0100

    Refactor: Code organization in regex module
    No functionality changed.

--- src/regex.rs
+++ src/regex.rs
@@ -17,50 +17,44 @@ pub fn regex_to_nfa(regex: &str) -> (StateRegister, u32) {
     postfix_regex_to_nfa(&postfix_regex)
 }
 
-/// Operator precedence for regex operators. Higher value
-/// means higher precedence.
-fn precedence(regex_operator: char) -> u32 {
-    match regex_operator {
-        '|' => 1,  // Alteration (or)
-        '~' => 3,  // Concatenation (and)
-        '*' => 4,  // Zero or more
-        '+' => 4,  // One or more
-        '?' => 4,  // Zero or one
-        _ => { panic!("Invalid regex operator") }
-    }
+// Types
+
+pub enum StateType {
+    Split,
+    Match,
+    Literal(char)
 }
 
-/// Pop from `from` into `to`. Returns `true` if there was anything to pop.
-fn pop_into(from: &mut Vec<char>, to: &mut String) -> bool {
-    let popped = from.pop();
+pub struct State {
+    state_type: StateType,
 
-    if popped.is_some() {
-        to.push(popped.unwrap());
-        return true;
-    }
-    return false;
+    // The states at the ends of the outgoing arrows of this state, if any
+    // These states are referenced by their id, to find the actual states
+    // one must go via the StateRegister.
+    out: Vec<Option<u32>>
 }
 
-/// Pop from `from` into `to` if predicate returns true on the last element
-/// of `from`. Returns true if the move happened.
-fn pop_into_if<F>(from: &mut Vec<char>, to: &mut String, predicate: &F) -> bool where
-    F: Fn(char) -> bool {
-    if from.len() == 0 {
-        return false;
-    }
-
-    if predicate(from[from.len() - 1]) {
-        return pop_into(from, to);
-    }
-    return false;
+/// The state register contains and owns states. The states are accessed
+/// through it, and it manages the lifetime of the states.
+pub struct StateRegister {
+    states: HashMap<u32, State>,
+    current_id: u32
 }
 
-/// Pop from `from` into `to` while predicate returns true on the last element
-/// of `from` or until the `from` vector is empty.
-fn pop_into_while<F>(from: &mut Vec<char>, to: &mut String, predicate: &F) where
-    F: Fn(char) -> bool {
-    while pop_into_if(from, to, predicate) {}
+/// A graph of states with a single input state. Represented
+/// by the single input state and all end states. End states are
+/// states with free (that is, `None`) out pointers.
+/// The entire graph can be travesed knowing the start state by following
+/// the out pointers until reaching all the end states.
+/// States are represented by their unique id in reference to a
+/// given StateRegister.
+/// Represents a fragment of a finite automaton.
+struct Fragment {
+    start: u32,
+    ends: Vec<u32>
 }
+
+// Main algorithms
 
 /// Use the Shunting-yard algorithm to convert a regex written in
 /// infix notation to a postifix regex.
@@ -203,186 +197,6 @@ fn regex_infix_to_postfix(regex: &str) -> String {
     output
 }
 
-
-pub enum StateType {
-    Split,
-    Match,
-    Literal(char)
-}
-
-pub struct State {
-    state_type: StateType,
-
-    // The states at the ends of the outgoing arrows of this state, if any
-    // These states are referenced by their id, to find the actual states
-    // one must go via the StateRegister.
-    out: Vec<Option<u32>>,
-
-    // lastlist: u32
-}
-
-impl State {
-    fn remove_from_out(&mut self, id: u32) {
-        self.out.retain(|val| *val != Some(id));
-    }
-}
-
-impl fmt::Display for State {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self.state_type {
-            StateType::Literal(c) => write!(f, "Literal ({}) ->", c),
-            StateType::Split => write!(f, "Split <- () ->"),
-            StateType::Match => write!(f, "MATCH ()")
-        }
-    }
-}
-
-/// The state register contains and owns states. The states are accessed
-/// through it, and it manages the lifetime of the states.
-pub struct StateRegister {
-    states: HashMap<u32, State>,
-    current_id: u32
-}
-
-impl StateRegister {
-    fn new() -> StateRegister {
-        StateRegister {
-            states: HashMap::new(),
-            current_id: 0
-        }
-    }
-
-    /// Get a state by id, panic if it does not exist.
-    fn get_state(&self, state_id: u32) -> &State {
-        let ref_to_state_or_none = self.states.get(&state_id);
-
-        if ref_to_state_or_none.is_some() {
-            return ref_to_state_or_none.unwrap()
-        }
-        panic!("No state with id {}", state_id);
-    }
-
-    /// Get a mutable state by id, panic if it does not exist.
-    fn get_mut_state(&mut self, state_id: u32) -> &mut State {
-        let mut_ref_to_state_or_none = self.states.get_mut(&state_id);
-
-        if mut_ref_to_state_or_none.is_some() {
-            return mut_ref_to_state_or_none.unwrap()
-        }
-        panic!("No state with id {}", state_id);
-    }
-
-    /// Remove a state from the out states of a state
-    fn remove_from_out(&mut self, state_id: u32, state_to_remove: u32) {
-        let state = self.get_mut_state(state_id);
-        state.remove_from_out(state_to_remove);
-    }
-
-    /// Connect all the unconnected (`None`) out
-    /// connections of a state to some state.
-    fn connect_dangling_outs_to_state(&mut self, state_id: u32, to_state: u32) {
-        let mut state = self.get_mut_state(state_id);
-
-        state.out = state.out.iter().map(
-            |val| {
-                if val.is_some() {
-                    return *val;
-                }
-                return Some(to_state);
-            }
-        ).collect();
-    }
-
-    /// Register a new state.
-    /// Return the unique id of that state.
-    fn new_state(&mut self, state_type: StateType, out: Vec<Option<u32>>) -> u32 {
-        self.states.insert(
-            self.current_id,
-            State { state_type, out }
-        );
-
-        // Increment the current id, this ensured the id of each state is
-        // unique.
-        self.current_id += 1;
-        self.current_id - 1
-    }
-
-    fn new_literal(&mut self, contains: char, out_state: Option<u32>) -> u32 {
-        self.new_state(
-            StateType::Literal(contains),
-            vec![out_state]
-        )
-    }
-
-    fn new_split(&mut self, out_state_1: Option<u32>, out_state_2: Option<u32>) -> u32 {
-        self.new_state(
-            StateType::Split,
-            vec![out_state_1, out_state_2]
-        )
-    }
-
-    fn match_state(&mut self) -> u32 {
-        self.new_state(
-            StateType::Match,
-            vec![]
-        )
-    }
-
-    /// Get all states that point to a given state. Note that since
-    /// the states only store the states they point to, not the states
-    /// that point to it this requires iterating over all states.
-    fn in_states(&self, state_id: u32) -> Vec<u32> {
-        let mut result: Vec<u32> = Vec::new();
-
-        for (id, state) in self.states.iter() {
-            if state.out.iter().find(|&&x| x == Some(state_id)).is_some() {
-                result.push(*id);
-            }
-        }
-
-        result
-    }
-
-    /// Remove a state and all references to it from the register.
-    /// This requires finding all states that point to the given state
-    /// and removing the state from their out states list.
-    fn remove_state(&mut self, state_to_remove: u32) {
-        let all_ids: Vec<u32> = self.states.iter().map(|(id, _)| *id).collect();
-
-        for id in all_ids {
-            self.remove_from_out(id, state_to_remove);
-        }
-
-        self.states.remove(&state_to_remove);
-    }
-}
-
-
-/// A graph of states with a single input state. Represented
-/// by the single input state and all end states. End states are
-/// states with free (that is, `None`) out pointers.
-/// The entire graph can be travesed knowing the start state by following
-/// the out pointers until reaching all the end states.
-/// States are represented by their unique id in reference to a
-/// given StateRegister.
-/// Represents a fragment of a finite automaton.
-struct Fragment {
-    start: u32,
-    ends: Vec<u32>
-}
-
-
-impl Fragment {
-    /// Attach all the unattached (`None`) outgoing lines of all the end states
-    /// of the fragment to a given state.
-    fn connect_ends(&self, to_state: u32, register: &mut StateRegister) {
-        for end in &self.ends[..] {
-            register.connect_dangling_outs_to_state(*end, to_state);
-        }
-    }
-}
-
-
 /// Convert a regular expression in postfix notation to a NFA.
 /// The NFA is represented by states in a state register, which is returned
 /// together with the id of the start state of the NFA.
@@ -514,63 +328,6 @@ pub fn postfix_regex_to_nfa(postfix_regex: &str) -> (StateRegister, u32) {
     }
 }
 
-/// Traverse a NFA given by a start state and state register to which it
-/// belongs, printing the nodes as we go along.
-/// Only used for debugging the creation of the NFA.
-pub fn print_nfa(start_id: u32, register: &StateRegister, visited: &mut HashSet<u32>) {
-    let start = register.get_state(start_id);
-
-    // To avoid infinte recursion we need to remember the states we
-    // have already visited.
-    if visited.contains(&start_id) {
-        println!("{}", start);
-        return;
-    }
-
-    visited.insert(start_id);
-
-    match start.state_type {
-        StateType::Match => {
-            println!("{}", start);
-        },
-        _ => {
-            println!("{}", start);
-            for out_state_id in &start.out[..] {
-                if out_state_id.is_some() {
-                    print_nfa(out_state_id.unwrap(), register, visited);
-                }
-            }
-        }
-    }
-}
-
-fn insert_or_follow_split(into: &mut HashSet<u32>, state: &State, state_id: u32, register: &StateRegister) {
-    match state.state_type {
-        StateType::Split => {
-            let state_id_split_1 = state.out[0].unwrap();
-            let state_split_1: &State = register.get_state(state_id_split_1);
-            insert_or_follow_split(
-                into,
-                state_split_1,
-                state_id_split_1,
-                register
-            );
-
-            let state_id_split_2 = state.out[1].unwrap();
-            let state_split_2: &State = register.get_state(state_id_split_2);
-            insert_or_follow_split(
-                into,
-                state_split_2,
-                state_id_split_2,
-                register
-            );
-        },
-        _ => {
-            into.insert(state_id);
-        }
-    }
-}
-
 pub fn simulate_nfa(input: &str, start: u32, register: &StateRegister) -> bool {
     // Current states the NFA is in.
     // A hash set since it should not contain the same state twice.
@@ -631,6 +388,251 @@ pub fn simulate_nfa(input: &str, start: u32, register: &StateRegister) -> bool {
     return false;
 }
 
+// Implementations
+
+impl State {
+    fn remove_from_out(&mut self, id: u32) {
+        self.out.retain(|val| *val != Some(id));
+    }
+}
+
+impl StateRegister {
+    fn new() -> StateRegister {
+        StateRegister {
+            states: HashMap::new(),
+            current_id: 0
+        }
+    }
+
+    /// Get a state by id, panic if it does not exist.
+    fn get_state(&self, state_id: u32) -> &State {
+        let ref_to_state_or_none = self.states.get(&state_id);
+
+        if ref_to_state_or_none.is_some() {
+            return ref_to_state_or_none.unwrap()
+        }
+        panic!("No state with id {}", state_id);
+    }
+
+    /// Get a mutable state by id, panic if it does not exist.
+    fn get_mut_state(&mut self, state_id: u32) -> &mut State {
+        let mut_ref_to_state_or_none = self.states.get_mut(&state_id);
+
+        if mut_ref_to_state_or_none.is_some() {
+            return mut_ref_to_state_or_none.unwrap()
+        }
+        panic!("No state with id {}", state_id);
+    }
+
+    /// Remove a state from the out states of a state
+    fn remove_from_out(&mut self, state_id: u32, state_to_remove: u32) {
+        let state = self.get_mut_state(state_id);
+        state.remove_from_out(state_to_remove);
+    }
+
+    /// Connect all the unconnected (`None`) out
+    /// connections of a state to some state.
+    fn connect_dangling_outs_to_state(&mut self, state_id: u32, to_state: u32) {
+        let mut state = self.get_mut_state(state_id);
+
+        state.out = state.out.iter().map(
+            |val| {
+                if val.is_some() {
+                    return *val;
+                }
+                return Some(to_state);
+            }
+        ).collect();
+    }
+
+    /// Register a new state.
+    /// Return the unique id of that state.
+    fn new_state(&mut self, state_type: StateType, out: Vec<Option<u32>>) -> u32 {
+        self.states.insert(
+            self.current_id,
+            State { state_type, out }
+        );
+
+        // Increment the current id, this ensured the id of each state is
+        // unique.
+        self.current_id += 1;
+        self.current_id - 1
+    }
+
+    fn new_literal(&mut self, contains: char, out_state: Option<u32>) -> u32 {
+        self.new_state(
+            StateType::Literal(contains),
+            vec![out_state]
+        )
+    }
+
+    fn new_split(&mut self, out_state_1: Option<u32>, out_state_2: Option<u32>) -> u32 {
+        self.new_state(
+            StateType::Split,
+            vec![out_state_1, out_state_2]
+        )
+    }
+
+    fn match_state(&mut self) -> u32 {
+        self.new_state(
+            StateType::Match,
+            vec![]
+        )
+    }
+
+    /// Get all states that point to a given state. Note that since
+    /// the states only store the states they point to, not the states
+    /// that point to it this requires iterating over all states.
+    fn in_states(&self, state_id: u32) -> Vec<u32> {
+        let mut result: Vec<u32> = Vec::new();
+
+        for (id, state) in self.states.iter() {
+            if state.out.iter().find(|&&x| x == Some(state_id)).is_some() {
+                result.push(*id);
+            }
+        }
+
+        result
+    }
+
+    /// Remove a state and all references to it from the register.
+    /// This requires finding all states that point to the given state
+    /// and removing the state from their out states list.
+    fn remove_state(&mut self, state_to_remove: u32) {
+        let all_ids: Vec<u32> = self.states.iter().map(|(id, _)| *id).collect();
+
+        for id in all_ids {
+            self.remove_from_out(id, state_to_remove);
+        }
+
+        self.states.remove(&state_to_remove);
+    }
+}
+
+impl fmt::Display for State {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self.state_type {
+            StateType::Literal(c) => write!(f, "Literal ({}) ->", c),
+            StateType::Split => write!(f, "Split <- () ->"),
+            StateType::Match => write!(f, "MATCH ()")
+        }
+    }
+}
+
+impl Fragment {
+    /// Attach all the unattached (`None`) outgoing lines of all the end states
+    /// of the fragment to a given state.
+    fn connect_ends(&self, to_state: u32, register: &mut StateRegister) {
+        for end in &self.ends[..] {
+            register.connect_dangling_outs_to_state(*end, to_state);
+        }
+    }
+}
+
+// Utility functions
+
+/// Operator precedence for regex operators. Higher value
+/// means higher precedence.
+fn precedence(regex_operator: char) -> u32 {
+    match regex_operator {
+        '|' => 1,  // Alteration (or)
+        '~' => 3,  // Concatenation (and)
+        '*' => 4,  // Zero or more
+        '+' => 4,  // One or more
+        '?' => 4,  // Zero or one
+        _ => { panic!("Invalid regex operator") }
+    }
+}
+
+/// Pop from `from` into `to`. Returns `true` if there was anything to pop.
+fn pop_into(from: &mut Vec<char>, to: &mut String) -> bool {
+    let popped = from.pop();
+
+    if popped.is_some() {
+        to.push(popped.unwrap());
+        return true;
+    }
+    return false;
+}
+
+/// Pop from `from` into `to` if predicate returns true on the last element
+/// of `from`. Returns true if the move happened.
+fn pop_into_if<F>(from: &mut Vec<char>, to: &mut String, predicate: &F) -> bool where
+    F: Fn(char) -> bool {
+    if from.len() == 0 {
+        return false;
+    }
+
+    if predicate(from[from.len() - 1]) {
+        return pop_into(from, to);
+    }
+    return false;
+}
+
+/// Pop from `from` into `to` while predicate returns true on the last element
+/// of `from` or until the `from` vector is empty.
+fn pop_into_while<F>(from: &mut Vec<char>, to: &mut String, predicate: &F) where
+    F: Fn(char) -> bool {
+    while pop_into_if(from, to, predicate) {}
+}
+
+/// Traverse a NFA given by a start state and state register to which it
+/// belongs, printing the nodes as we go along.
+/// Only used for debugging the creation of the NFA.
+pub fn print_nfa(start_id: u32, register: &StateRegister, visited: &mut HashSet<u32>) {
+    let start = register.get_state(start_id);
+
+    // To avoid infinte recursion we need to remember the states we
+    // have already visited.
+    if visited.contains(&start_id) {
+        println!("{}", start);
+        return;
+    }
+
+    visited.insert(start_id);
+
+    match start.state_type {
+        StateType::Match => {
+            println!("{}", start);
+        },
+        _ => {
+            println!("{}", start);
+            for out_state_id in &start.out[..] {
+                if out_state_id.is_some() {
+                    print_nfa(out_state_id.unwrap(), register, visited);
+                }
+            }
+        }
+    }
+}
+
+fn insert_or_follow_split(into: &mut HashSet<u32>, state: &State, state_id: u32, register: &StateRegister) {
+    match state.state_type {
+        StateType::Split => {
+            let state_id_split_1 = state.out[0].unwrap();
+            let state_split_1: &State = register.get_state(state_id_split_1);
+            insert_or_follow_split(
+                into,
+                state_split_1,
+                state_id_split_1,
+                register
+            );
+
+            let state_id_split_2 = state.out[1].unwrap();
+            let state_split_2: &State = register.get_state(state_id_split_2);
+            insert_or_follow_split(
+                into,
+                state_split_2,
+                state_id_split_2,
+                register
+            );
+        },
+        _ => {
+            into.insert(state_id);
+        }
+    }
+}
+
 /// pop the last element from a vector, panic if empty
 fn pop_or_panic<T>(vector: &mut Vec<T>, panic_message: Option<&'static str>) -> T {
     let result: Option<T> = vector.pop();
@@ -642,6 +644,8 @@ fn pop_or_panic<T>(vector: &mut Vec<T>, panic_message: Option<&'static str>) -> 
         _ => panic!("Attempted pop of empty vector.")
     }
 }
+
+// Tests
 
 #[cfg(test)]
 mod tests {
