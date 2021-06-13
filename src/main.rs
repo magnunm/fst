bcbbc7f592bb6e58a0a94c9e8fb1971a1e14d451
Magnus N Malmquist <magnusnym97@gmail.com>
2021-06-13 22:12:29 +0200

    Basic argument parsing and file reading
    Combined with the regex functionality we have the first working cli,
    with the core functionality of the program.

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,45 @@
-use std::io::{self, BufRead};
+use std::io::{self, BufReader, BufRead};
+use std::fs::File;
 
+use clap::{Arg, App};
 mod regex;
 
 
 fn main() -> io::Result<()> {
-    let regex = regex::Regex::new("(a|⻘)c+", true).unwrap();
+    let matches = App::new("fst")
+        .about("A stream filter")
+        .arg(Arg::with_name("PATTERN")
+             .help("The pattern to match")
+             .required(true)
+             .index(1))
+        .arg(Arg::with_name("FILE")
+             .help("The file to search")
+             .required(true)
+             .index(2))
+        .get_matches();
 
-    let stdin = io::stdin();
-    let mut input = stdin.lock().lines().next().unwrap().unwrap();
+    let pattern = matches.value_of("PATTERN").unwrap();
+    let regex = regex::Regex::new(&pattern, true).unwrap();
 
-    while input != "q" {
-        let (match_start, match_end) = regex.match_substring(&input);
+    let file_name = matches.value_of("FILE").unwrap();
+    let file = File::open(&file_name)?;
 
-        println!("Match from {} to {}", match_start, match_end);
+    let mut reader = BufReader::new(file);
+    let mut line = String::new();
 
-        input = stdin.lock().lines().next().unwrap().unwrap();
+    loop {
+        let bytes_read = reader.read_line(&mut line)?;
+        if bytes_read == 0 { break; }
+
+        let (match_start, match_end) = regex.match_substring(&line);
+
+        if match_start != match_end {
+            // Strip final newline character
+            println!("{}", &line[..(bytes_read - 1)])
+        }
+
+        line.clear();
     }
+
     Ok(())
 }
